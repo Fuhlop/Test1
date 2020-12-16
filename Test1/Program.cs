3b1e719df18b30bda7f165e09427a09fc9b40274
Phillip Youssef <Phillip Youssef@DESKTOP-9ENO0TT>
2020-12-15 23:57:18 -0600

    Add project files.

---
+++ Test1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int x = 0; x <= 4; x++)
+            {
+                Console.WriteLine();
+                //for(int y = 0; y < x; y++)
+                //{
+                //    Console.Write("*");
+                //}     
+                for(int z = 0; z < x ; z++)
+                {
+                    Console.Write("*");
+                }
+
+
+
+            }
+        }
+    }
+}
